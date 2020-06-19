b88cdc268124978c5aa29490ed89586b9379ce79
0xBAMA <jon.baker1222@gmail.com>
2020-06-19 19:08:59 -0400

    figured out unsigned image types

--- resources/code/shaders/agent.fs.glsl
+++ resources/code/shaders/agent.fs.glsl
@@ -1,1 +1,8 @@
 #version 430 core
+
+out vec4 fragment_output;
+
+void main()
+{
+	fragment_output = vec4(1,0,0,1);	
+}
--- resources/code/shaders/agent.vs.glsl
+++ resources/code/shaders/agent.vs.glsl
@@ -1,1 +1,19 @@
 #version 430 core
+
+uniform layout( r16ui ) uimage2D previous;
+uniform layout( r16ui ) uimage2D current;
+
+layout( binding = 0, std430 ) buffer agent_data
+{
+	vec2 data[];	// [position0, direction0], [position1, direction1], ...
+};
+
+void main()
+{
+	int index = gl_VertexID * 2;
+	
+	vec2 position = data[index];
+	vec2 direction = data[index+1];
+
+	gl_Position = vec4(position.x, position.y, 0, 1);
+}
